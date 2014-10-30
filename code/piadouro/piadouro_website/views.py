aa45f20ad3c7fdf1f5373f24e4e5675a6022d44a
Gabriel Cavalcante <gabriel@las.ic.unicamp.br>
2014-10-30 17:43:29 -0200

    correting settings an creating templates and views

--- code/piadouro/piadouro_website/views.py
+++ code/piadouro/piadouro_website/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 # Create your views here.
 def home(request):
-  return HttpResponse("Beep, Beep, Beeep! I'm alive!")
+  return render_to_response("piadouro_website/home.html", 
+                  {"hello": "Beep, Beep, Beeep! I'm alive!"})
+
