107781f128d251968fa882dd13468c101a65b0e2
Gabriel Cavalcante <gabriel@las.ic.unicamp.br>
2014-10-31 00:46:25 -0200

    Finished models, views and admin

--- code/piadouro/piadouro/settings.py
+++ code/piadouro/piadouro/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'piadouro_website',
 )
 
 MIDDLEWARE_CLASSES = (
--- code/piadouro/piadouro_website/views.py
+++ code/piadouro/piadouro_website/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
+#Importing piado_website models
+from piadouro_website.models import Piado
+
 # Create your views here.
 def home(request):
   return render_to_response("piadouro_website/home.html", 
-                  {"hello": "Beep, Beep, Beeep! I'm alive!"})
+                  { "piados" : Piado.objects.all() })
 
